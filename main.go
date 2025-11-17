c169ee75f50588cf8462af2c3114eb7820eb1673
abeni-al7 <abenezeralebachew3@gmail.com>
2025-11-17 20:36:39 +0300

    [feat] Get file size

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if (args[0] == "-c") {
+		path := args[1]
+		fileSize, err := getFileSize(path)
+		if err != nil {
+			println("There was a problem getting the size of the file")
+			return
+		}
+		fmt.Printf("%d %s\n", fileSize, path)
+	}
+}
+
+func getFileSize(path string) (int64, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return 0, err
+	}
+	return fileInfo.Size(), nil
+}
